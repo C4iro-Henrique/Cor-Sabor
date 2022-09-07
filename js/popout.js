cc88facef5fa5ea81722965512db9b248b131200
Takh4n <Cairohenrique.cht@gmail.com>
2022-09-07 12:38:40 -0300

    colocando hud

---
+++ js/popout.js
@@ -0,0 +1,12 @@
+function abrirPopout(element){
+    var popout = document.querySelector(`[data-pop=${element}]`);
+    popout.classList.toggle('descer');
+
+}
+
+var btbotao = document.querySelectorAll("[data-botao]");
+btbotao.forEach(element => {
+    console.log(element.dataset.botao);
+    element.addEventListener("click", () => {abrirPopout(element.dataset.botao)});
+
+})
